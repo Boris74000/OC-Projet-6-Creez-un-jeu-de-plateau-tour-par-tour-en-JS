25978ee41016d9de42a08a6f3ac75f99be220ced
Charles-Boris DAUVIER <dauvierboris@hotmail.com>
2021-07-02 19:38:52 +0200

    Changement syntaxe invocations et personnages

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 let mapBoardGame = new Map(10,10);
 
-let chocoMog = new Invocation("ChocoMog", 10);
-let shiva = new Invocation("Shiva", 15);
-let titan = new Invocation("Titan", 20);
-let odin = new Invocation("Odin", 25);
-let knightsOfTheRoundTable = new Invocation("Knights of the Round Table", 30);
+let chocoMog = new Invocation("chocoMog", 10);
+let shiva = new Invocation("shiva", 15);
+let titan = new Invocation("titan", 20);
+let odin = new Invocation("odin", 25);
+let knightsOfTheRoundTable = new Invocation("knightsOfTheRoundTable", 30);
 
-let cloud = new Character("Cloud", 100, chocoMog);
-let sephiroth = new Character("Sephiroth", 100, chocoMog);
+let cloud = new Character("cloud", 100, chocoMog.nameInvocation);
+let sephiroth = new Character("sephiroth", 100, chocoMog.nameInvocation);
