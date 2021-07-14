396f41405fbef8178eba30b9712262becd3f2bf4
Charles-Boris DAUVIER <dauvierboris@hotmail.com>
2021-07-14 19:19:26 +0200

    Ajout instanciation class Game et methode

--- js/main.js
+++ js/main.js
@@ -6,5 +6,16 @@ let titan = new Invocation("titan", 20);
 let odin = new Invocation("odin", 25);
 let knightsOfTheRoundTable = new Invocation("knightsOfTheRoundTable", 30);
 
-let cloud = new Character("cloud", 100, chocoMog.nameInvocation);
-let sephiroth = new Character("sephiroth", 100, chocoMog.nameInvocation);
+// chocoMog.test();
+
+let cloud = new Character("cloud", 100, chocoMog.nameInvocation, true);
+let sephiroth = new Character("sephiroth", 100, chocoMog.nameInvocation, false);
+
+let newGame = new Game();
+
+newGame.getCurrentPlayerPosition();
+newGame.generateWayVerticallyUp();
+newGame.generateWayVerticallyDown();
+newGame.generateWayHorizontallyLeft();
+newGame.generateWayHorizontallyRight();
+newGame.moveCharacter();
