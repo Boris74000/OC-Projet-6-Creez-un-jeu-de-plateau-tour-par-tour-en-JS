f87cd1ba2210fd37b32f014ffaf93eae290eb136
Charles-Boris DAUVIER <dauvierboris@hotmail.com>
2021-08-04 09:17:06 +0200

    Ajout booléen mode défense

--- js/Character.js
+++ js/Character.js
@@ -1,10 +1,11 @@
 class Character{
 
-    constructor(nameCharacter, health, invocation, isCurrentPlayer) {
+    constructor(nameCharacter, health, invocation, isCurrentPlayer, defenseMode) {
         this.nameCharacter = nameCharacter;
         this.health = health;
         this.invocation = invocation;
         this.isCurrentPlayer = isCurrentPlayer;
+        this.defenseMode = defenseMode;
     }
 
 
