0ca6a630a59e25cf86203a1b11815993f0233346
Charles-Boris DAUVIER <dauvierboris@hotmail.com>
2021-08-04 09:18:17 +0200

    On instancie en dernier la carte

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
-let mapBoardGame = new Map(10,10);
-
 let chocoMog = new Invocation("chocoMog", 10);
 let shiva = new Invocation("shiva", 15);
 let titan = new Invocation("titan", 20);
 let odin = new Invocation("odin", 25);
 let knightsOfTheRoundTable = new Invocation("knightsOfTheRoundTable", 30);
 
-let cloud = new Character("cloud", 100, chocoMog.nameInvocation, true);
-let sephiroth = new Character("sephiroth", 100, chocoMog.nameInvocation, false);
+let cloud = new Character("cloud", 100, chocoMog.nameInvocation, true, false);
+let sephiroth = new Character("sephiroth", 100, chocoMog.nameInvocation, false, false);
+
+let mapBoardGame = new Map(10,10);
 
 let newGame = new Game();
 
