236b1b76a40ccdd7e0396bab87c53a87b1439a84
Charles-Boris DAUVIER <dauvierboris@hotmail.com>
2021-08-04 13:54:35 +0200

    Remplacement "nom de l'invocation" par l'objet "invocation" dans l'instanciation des personnages

--- js/main.js
+++ js/main.js
@@ -4,8 +4,8 @@ let titan = new Invocation("titan", 20);
 let odin = new Invocation("odin", 25);
 let knightsOfTheRoundTable = new Invocation("knightsOfTheRoundTable", 30);
 
-let cloud = new Character("cloud", 100, chocoMog.nameInvocation, true, false);
-let sephiroth = new Character("sephiroth", 100, chocoMog.nameInvocation, false, false);
+let cloud = new Character("cloud", 100, chocoMog, true, false);
+let sephiroth = new Character("sephiroth", 100, chocoMog, false, false);
 
 let mapBoardGame = new Map(10,10);
 
