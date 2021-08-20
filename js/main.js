ab2e105adbaf953780ea421a03750baba2e741ca
Charles-Boris DAUVIER <dauvierboris@hotmail.com>
2021-08-20 16:19:56 +0200

    Ajout 2 fonctions

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ let chocoMog = new Invocation("chocoMog", 10);
 let shiva = new Invocation("shiva", 15);
 let titan = new Invocation("titan", 20);
 let odin = new Invocation("odin", 25);
-let knightsOfTheRoundTable = new Invocation("knightsOfTheRoundTable", 30);
+let knightsOfTheRoundTable = new Invocation("Chevalier de la table ronde", 30);
 
 let cloud = new Character("cloud", 100, chocoMog, true, false);
 let sephiroth = new Character("sephiroth", 100, chocoMog, false, false);
@@ -11,9 +11,13 @@ let mapBoardGame = new Map(10,10);
 
 let newGame = new Game();
 
+newGame.createDivCharacters();
+newGame.hideStartGameScreen();
 newGame.getCurrentPlayerPosition();
 newGame.generateWayVerticallyUp();
 newGame.generateWayVerticallyDown();
 newGame.generateWayHorizontallyLeft();
 newGame.generateWayHorizontallyRight();
 newGame.checkElClicked();
+
+
