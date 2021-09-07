d9d7d38394c2dc380541529bde850a46928fa96d
Charles-Boris DAUVIER <dauvierboris@hotmail.com>
2021-09-08 00:45:43 +0200

    Ajout fonction audio

--- js/GameMusic.js
+++ js/GameMusic.js
@@ -2,8 +2,12 @@
 
 class GameMusic {
     constructor() {
-        this.playMusic();
-        this.stopMusic();
+        // this.playMusicPrelude();
+        // this.stopMusicPrelude();
+        // this.playAudioFighting();
+        // this.stopAudioFighting();
+        this.events();
+        // this.audioFighting = new Audio('audio/fighting.mp3');
         // this.audioPrelude = new Audio('audio/ff7_prelude.mp3');
     }
 
@@ -14,13 +18,19 @@ class GameMusic {
     }
 
     playAudioFighting() {
-        this.audioFighting = new Audio('audio/fighting.mp3');
-        this.audioFighting.play();
-        this.audioFighting.volume = 0.2;
+        document.getElementById("audioFileFighting").play();
+        document.getElementById("audioFileFighting").volume = 0.2;
+        document.getElementById("musicFightingOn").style.display = "none";
+        document.getElementById("musicFightingOff").style.display = "flex";
+        // this.audioFighting.play();
+        // this.audioFighting.volume = 0.2;
     }
 
     stopAudioFighting() {
-        this.audioFighting.pause();
+        // this.audioFighting.pause();
+        document.getElementById("audioFileFighting").pause();
+        document.getElementById("musicFightingOn").style.display = "flex";
+        document.getElementById("musicFightingOff").style.display = "none";
     }
 
     playAudioVictory() {
@@ -35,20 +45,34 @@ class GameMusic {
         document.getElementById("audioFileCursor").volume = 0.2;
     }
 
-    playMusic() {
-        document.getElementById("musicOn").addEventListener("click", function () {
-            document.getElementById("audioFilePrelude").play();
-            document.getElementById("audioFilePrelude").volume = 0.2;
-            document.getElementById("musicOn").style.display = "none";
-            document.getElementById("musicOff").style.display = "block";
-        })
+    playMusicPrelude() {
+        document.getElementById("audioFilePrelude").play();
+        document.getElementById("audioFilePrelude").volume = 0.2;
+        document.getElementById("musicPreludeOn").style.display = "none";
+        document.getElementById("musicPreludeOff").style.display = "flex";
     }
 
-    stopMusic() {
-        document.getElementById("musicOff").addEventListener("click", function () {
-            document.getElementById("audioFilePrelude").pause();
-            document.getElementById("musicOff").style.display = "none";
-            document.getElementById("musicOn").style.display = "block";
-        })
+    stopMusicPrelude() {
+        document.getElementById("audioFilePrelude").pause();
+        document.getElementById("musicPreludeOff").style.display = "none";
+        document.getElementById("musicPreludeOn").style.display = "flex";
     }
+
+    hideBtnMusicPrelude() {
+        document.getElementById("musicPreludeOff").style.display = "none";
+        document.getElementById("musicPreludeOn").style.display = "none";
+    }
+
+    displayBtnMusicFighting() {
+        document.getElementById("musicFightingOff").style.display = "flex";
+        document.getElementById("musicPreludeOn").style.display = "none";
+    }
+
+    events() {
+        document.getElementById("musicPreludeOff").addEventListener("click", this.stopMusicPrelude);
+        document.getElementById("musicPreludeOn").addEventListener("click", this.playMusicPrelude);
+        document.getElementById("musicFightingOff").addEventListener("click", this.stopAudioFighting);
+        document.getElementById("musicFightingOn").addEventListener("click", this.playAudioFighting);
+    }
+
 }
